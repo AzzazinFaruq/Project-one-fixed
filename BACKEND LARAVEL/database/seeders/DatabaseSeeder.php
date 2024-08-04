35d388c1a037ad4d6c9a1775abd54b4795c6d2c9
Rizang <rizangofficial@gmail.com>
2024-08-04 14:29:58 +0700

    merge

--- BACKEND LARAVEL/database/seeders/DatabaseSeeder.php
+++ BACKEND LARAVEL/database/seeders/DatabaseSeeder.php
@@ -20,29 +20,29 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@user.com',
-            'level'=> 'superAdmin',
+            'level' => 'superAdmin',
             'password' => Hash::make('password'),
         ]);
         \App\Models\Penduduk::factory(10)->create([
             'nomer_kk' => Str::random(10),
-            'nik'=>Str::random(10),
-            'nama'=>Str::random(10),
-            'tmp_lhr'=>Str::random(10),
-            'tgl_lhr'=>'2022-9-12',
-            'kelamin'=>1,
-            'stat_kawin'=>1,
-            'hub_kel'=>1,
-            'warga_neg'=>1,
-            'agama'=>1,
-            'pendidikan'=>1,
-            'pekerjaan'=>1,
-            'ayah'=>1,
-            'ibu'=>1,
-            'kepala_kel' =>Str::random(10),
-            'no_hp' =>"00112233",
-            'domisili'=> Str::random(10),
-            'stat' =>1
-            ]);
-
+            'kels_id' => 1,
+            'nik' => Str::random(10),
+            'nama' => Str::random(10),
+            'tmp_lhr' => Str::random(10),
+            'tgl_lhr' => '2022-9-12',
+            'kelamin' => 1,
+            'stat_kawin' => 1,
+            'hub_kel' => 1,
+            'warga_neg' => 1,
+            'agama' => 1,
+            'pendidikan' => 1,
+            'pekerjaan' => 1,
+            'ayah' => 1,
+            'ibu' => 1,
+            'kepala_kel' => Str::random(10),
+            'no_hp' => "00112233",
+            'domisili' => Str::random(10),
+            'stat' => 1
+        ]);
     }
 }
